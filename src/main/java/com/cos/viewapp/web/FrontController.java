1e9df24311f7d0f73bee5f5f6ca4461d49da23bb
ssar <ssar@nate.com>
2021-09-02 11:51:27 +0900

    코드수정

--- src/main/java/com/cos/viewapp/web/FrontController.java
+++ src/main/java/com/cos/viewapp/web/FrontController.java
@@ -29,7 +29,7 @@ public class FrontController {
 		
 		List<Board> boards = new ArrayList<>();
 		boards.add(new Board(1, "제목1"));
-		boards.add(new Board(2, "제목2"));
+		boards.add(new Board(2, "제목3"));
 		
 		model.addAttribute("boards", boards);
 		
